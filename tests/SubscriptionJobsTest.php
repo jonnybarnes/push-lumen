b6ffd82548e90177d8628e6394411eb7a2609cf5
Jonny Barnes <jonny@jonnybarnes.uk>
2015-06-01 14:40:08 +0100

    Continue getting tests to work

--- tests/SubscriptionJobsTest.php
+++ tests/SubscriptionJobsTest.php
@@ -2,5 +2,17 @@
 
 class SubscriptionJobsTest extends TestCase
 {
-    //
+    public function testSendNotifications()
+    {
+        //we need to set up a mock guzzle client
+        $topicUrl = 'http://alice.com/notes';
+        $callbackUrl = 'http://bob.com/callback';
+        Cache::shouldReceive('get')
+                ->once()
+                ->andReturn([
+                    'Content-Type' => 'text/html'
+                ]);
+
+        $job = new App\Jobs\SendTopicUpdateNotification($topicUrl, $callbackUrl);
+    }
 }
