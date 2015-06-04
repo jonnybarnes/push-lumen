0c08ad31bd5e7487785d896dd87b141a23282133
Jonny Barnes <jonny@jonnybarnes.uk>
2015-06-04 11:16:52 +0100

    Add test for failed notification response

--- tests/SubscriptionJobsTest.php
+++ tests/SubscriptionJobsTest.php
@@ -4,25 +4,42 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Tests\Server;
 
 class SubscriptionJobsTest extends TestCase
 {
-    public function testSendNotifications()
+    public function setUp()
+    {
+        $this->topicUrl = 'http://alice.com/notes';
+        $this->callbackUrl = 'http://bob.com/callback';
+        Cache::put($this->topicUrl, ['Content-Type' => 'text/html'], 5);
+    }
+
+    public function testSendNotificationsSuccess()
     {
         //we need to set up a mock guzzle client
         $mock = new MockHandler([
-            new Response(200)
+            new Response(202)
         ]);
         $handler = HandlerStack::create($mock);
-        $client = new Client(['handler' => $mock]);
-        $topicUrl = 'http://alice.com/notes';
-        $callbackUrl = 'http://bob.com/callback';
-        //“mock” a Cache
-        Cache::put($topicUrl, ['Content-Type' => 'text/html'], 1);
+        $client = new Client(['handler' => $handler]);
 
-        $job = new App\Jobs\SendTopicUpdateNotification($topicUrl, $callbackUrl, $client);
+        $job = new App\Jobs\SendTopicUpdateNotification($this->topicUrl, $this->callbackUrl, $client);
         $job->handle();
+    }
 
-        return true;
+    /**
+     * @expectedException GuzzleHttp\Exception\ServerException
+     */
+    public function testSendNotificationsFailure()
+    {
+        $mock = new MockHandler([
+            new Response(500, ['Content-Length' => 0])
+        ]);
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+
+        $job = new App\Jobs\SendTopicUpdateNotification($this->topicUrl, $this->callbackUrl, $client);
+        $job->handle();
     }
 }
